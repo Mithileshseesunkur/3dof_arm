c415d036bbd7f95fb1d024da5fbe5e7eb1a7f9a2
Mithileshseesunkur <zavallingum@gmail.com>
2025-07-31 00:54:10 +0200

    stepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <TMCStepper.h>
+#include <HardwareSerial.h>
+
+
+BluetoothSerial serialBT;
+char cmd;
 
 // put function declarations here:
-#define pin 13
+#define TMC_SERIAL_PORT Serial2
+#define TMC_UART_RX_PIN 16
+#define TMC_UART_TX_PIN 17
+
+#define DRIVER_ADDRESS 0b00 // TMC2209 driver address
+#define R_SENSE 0.11f // Sense resistor value in ohms
+
+TMC2209Stepper driver(&TMC_SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin, OUTPUT);
+  serialBT.begin("esp32-bt");
+  pinMode(2, OUTPUT); // Set GPIO 2 as output for LED control
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(pin, HIGH);
-  delay(1000);
-  digitalWrite(pin, LOW);
-  delay(1000);
+  if (serialBT.available()){
+
+    cmd=serialBT.read();
+
+  }
+  if(cmd=='1'){
+
+    digitalWrite(2, HIGH);
+  }
+  if(cmd=='0'){
+
+    digitalWrite(2, LOW);
+  }
+
+  delay(100);
+  
 }
 
